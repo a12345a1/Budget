d888d12c579655dc7d9ed99580d70bcfae1e29cc
wajosong <a12345a12001@gmail.com>
2021-11-13 21:19:13 +0800

    通過跨越計算預算的測試案例

--- BudgetTestProject/BudgetService.cs
+++ BudgetTestProject/BudgetService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BudgetTestProject
 {
@@ -13,11 +15,33 @@ namespace BudgetTestProject
 
         public double Query(DateTime start, DateTime end)
         {
-            var totalDay = end.Day - start.Day + 1;
-            var totalDaysInMonth = DateTime.DaysInMonth(start.Year, start.Month);
-            var budget = _budgetRepo.GetAll().Find(x=>x.YearMonth == $"{start.Date.Year}{start.Date.Month}");
-            var amountPerDay = budget.Amount / totalDaysInMonth;
-            return amountPerDay * totalDay;
+            var money = 0;
+            var totalDay = GetAllDateTime(start, end);
+            var monthGroup = totalDay.GroupBy(x => x.Month);
+            using var enumerator = monthGroup.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current.First();
+                var totalDaysInMonth = DateTime.DaysInMonth(item.Year, item.Month);
+                var budget = GetBudget(item.Year, item.Month);
+                var amountPerDay = budget.Amount / totalDaysInMonth;
+                money += amountPerDay * enumerator.Current.Count();
+            }
+
+            return money;
+        }
+
+        private IEnumerable<DateTime> GetAllDateTime(DateTime start, DateTime end)
+        {
+            for (var dt = start; dt.CompareTo(end) <= 0; dt = dt.AddDays(1))
+            {
+                yield return dt;
+            }
+        }
+
+        private Budget GetBudget(int year, int month)
+        {
+            return _budgetRepo.GetAll().Find(x => x.YearMonth == $"{year}{month.ToString("00")}");
         }
     }
 }
